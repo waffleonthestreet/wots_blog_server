520ee23a9590d4b9376c5d9bbb9e325d57cc123f
waffleonthestreet <woojeongkim610@gmail.com>
2024-01-21 16:07:04 +0900

    changed cors origin

--- src/main/java/wots/blog/server/controller/BlogController.java
+++ src/main/java/wots/blog/server/controller/BlogController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@CrossOrigin(origins = "http://3.216.180.186/")
+@CrossOrigin(origins = "http://blog.waffleonthestreet.com/")
 @RequestMapping("/blog")
 public class BlogController {
     @Autowired
