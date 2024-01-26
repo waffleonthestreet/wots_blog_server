5f5e929500cae6fbca0b350302b35b2ed88ae85f
waffleonthestreet <woojeongkim610@gmail.com>
2024-01-26 18:21:39 +0900

    changed cors origin of admincontroller

--- src/main/java/wots/blog/server/controller/AdminController.java
+++ src/main/java/wots/blog/server/controller/AdminController.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 import java.util.List;
 
 @Slf4j
-@CrossOrigin(origins = "http://3.216.180.186:5001/")
+@CrossOrigin(origins = "https://admin.waffleonthestreet.com/")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
